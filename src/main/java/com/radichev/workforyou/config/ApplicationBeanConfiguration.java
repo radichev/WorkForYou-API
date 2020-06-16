8a49da294046bf04feec1e875136d2ed06151e7d
radichev <unbreaking78@gmail.com>
2020-06-16 13:24:33 +0300

    The normal modelmapper configuration has been returned

--- src/main/java/com/radichev/workforyou/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/radichev/workforyou/config/ApplicationBeanConfiguration.java
@@ -11,13 +11,7 @@ public class ApplicationBeanConfiguration {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-                .setMatchingStrategy(MatchingStrategies.STANDARD);
-
-        return modelMapper;
+        return new ModelMapper();
     }
 
     @Bean
