6392dc078c9530d34c75cda029c37cb79d2a715b
radichev <unbreaking78@gmail.com>
2020-06-16 15:02:06 +0300

    Added CrossOrigin to the UserProfileDetails controller

--- src/main/java/com/radichev/workforyou/api/UserProfileDetailsController.java
+++ src/main/java/com/radichev/workforyou/api/UserProfileDetailsController.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/user-profile-details")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserProfileDetailsController {
     private final UserProfileDetailsService userProfileDetailsService;
 
