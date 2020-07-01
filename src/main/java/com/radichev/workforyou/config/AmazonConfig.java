b7b43377d7cc36ebce25b2785f2baddf82e2852a
radichev <unbreaking78@gmail.com>
2020-07-01 15:35:27 +0300

    Added region to the Amazon config

--- src/main/java/com/radichev/workforyou/config/AmazonConfig.java
+++ src/main/java/com/radichev/workforyou/config/AmazonConfig.java
@@ -3,8 +3,10 @@ package com.radichev.workforyou.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Region;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +23,7 @@ public class AmazonConfig {
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.US_EAST_1)
                 .build();
     }
 }
