c2824ba76c4166490c66118439bce1bc1ae930d3
radichev <unbreaking78@gmail.com>
2020-07-03 20:43:30 +0300

    Added AWS toolkit for providing credentials

--- src/main/java/com/radichev/workforyou/api/UserProfileDetailsController.java
+++ src/main/java/com/radichev/workforyou/api/UserProfileDetailsController.java
@@ -3,7 +3,6 @@ package com.radichev.workforyou.api;
 import com.radichev.workforyou.model.bindingModels.editUserProfileDetails.UserProfileDetailsEditBindingModel;
 import com.radichev.workforyou.model.viewModels.getUserProfileDetails.UserProfileDetailsViewModel;
 import com.radichev.workforyou.service.UserProfileDetailsService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/radichev/workforyou/config/AmazonConfig.java
+++ src/main/java/com/radichev/workforyou/config/AmazonConfig.java
@@ -1,7 +1,5 @@
 package com.radichev.workforyou.config;
 
-import com.amazonaws.auth.*;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
@@ -12,17 +10,8 @@ public class AmazonConfig {
 
     @Bean
     public AmazonS3 amazonS3() {
-        BasicSessionCredentials basicSessionCredentials = new BasicSessionCredentials(
-                "ASIA5SN46AB7H3PMKNH2",
-                "1twFZZgdpn+o+aaU1z467qDQv0giHcPu234VCNs6",
-                "FwoGZXIvYXdzENT//////////wEaDDdeY1KUmSGzQ/mpryLLAdwm+owRbvdDNIAKarsVzIcrHNf6HVDkTcfKAStAOogNb7nUze0YcNVhDPZjQ2ygS1LoTZNflgbrmN1xjl2DuDuR8VBcs6QWpTfO01tnfA1ivUQO/YwxU4+iZ1FojIydrNu3+VXkM5c05XGHv2u386Y57cBny8UnGHtP8zuN+T1oJkI1lUTN6w42QfeCb9ayqMa2Yn94BpCLZU3wlJ/1DAW2IsOGOJtkUYEsmwH+sbGkkOnnSuBqK16A2j8PKlr7IcWp5lox0OOmS86xKNPy9vcFMi0K9Qmu30g+SZS2MgOkdpig1rPb6V+fPWlhFP44zx98kru4CSP2GHwDGAbQECA="
-        );
-
         return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(basicSessionCredentials))
-                .withRegion(Regions.US_EAST_1)
                 .build();
-
     }
 }
