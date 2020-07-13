76b7b7f8954efb3fdae7c820262f0af586dfb900
radichev <unbreaking78@gmail.com>
2020-07-13 18:34:41 +0300

    Added Education Binding Model

---
+++ src/main/java/com/radichev/workforyou/model/bindingModels/educationBindingModel/EducationBindingModel.java
@@ -0,0 +1,55 @@
+package com.radichev.workforyou.model.bindingModels.educationBindingModel;
+
+import com.radichev.workforyou.model.dtos.EducationDto.CountryDto;
+import com.radichev.workforyou.model.dtos.EducationDto.TitleTypeDto;
+
+public class EducationBindingModel {
+    private CountryDto country;
+    private String universityName;
+    private TitleTypeDto titleType;
+    private String educationSubject;
+    private int graduationYear;
+
+    public EducationBindingModel() {
+    }
+
+    public CountryDto getCountry() {
+        return country;
+    }
+
+    public void setCountry(CountryDto country) {
+        this.country = country;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+
+    public TitleTypeDto getTitleType() {
+        return titleType;
+    }
+
+    public void setTitleType(TitleTypeDto titleType) {
+        this.titleType = titleType;
+    }
+
+    public String getEducationSubject() {
+        return educationSubject;
+    }
+
+    public void setEducationSubject(String educationSubject) {
+        this.educationSubject = educationSubject;
+    }
+
+    public int getGraduationYear() {
+        return graduationYear;
+    }
+
+    public void setGraduationYear(int graduationYear) {
+        this.graduationYear = graduationYear;
+    }
+}
