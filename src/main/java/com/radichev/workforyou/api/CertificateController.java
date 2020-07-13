f7ab31644c24d17cc9f3c7343bd9af94fa06820d
radichev <unbreaking78@gmail.com>
2020-07-13 18:15:52 +0300

    Added Certificates Controller

---
+++ src/main/java/com/radichev/workforyou/api/CertificateController.java
@@ -0,0 +1,25 @@
+package com.radichev.workforyou.api;
+
+import com.radichev.workforyou.model.bindingModels.certificateBindingModel.CertificateBindingModel;
+import com.radichev.workforyou.service.CertificateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("api/certificates")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CertificateController {
+    private final CertificateService certificateService;
+
+    public CertificateController(CertificateService certificateService) {
+        this.certificateService = certificateService;
+    }
+
+    @PostMapping("/add/{userId}")
+    public ResponseEntity<Void> addLanguage(@PathVariable String userId, @Valid @RequestBody CertificateBindingModel certificateBindingModel){
+        this.certificateService.addCertificate(certificateBindingModel, userId);
+        return ResponseEntity.ok().build();
+    }
+}
