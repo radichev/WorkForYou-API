c6e602943f6a256f73f3b7a9a3b38f5ecdecd1f3
radichev <unbreaking78@gmail.com>
2020-07-14 20:28:14 +0300

    Added Job service

---
+++ src/main/java/com/radichev/workforyou/service/serviceImpl/JobServiceImpl.java
@@ -0,0 +1,8 @@
+package com.radichev.workforyou.service.serviceImpl;
+
+import com.radichev.workforyou.service.JobService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobServiceImpl implements JobService {
+}
