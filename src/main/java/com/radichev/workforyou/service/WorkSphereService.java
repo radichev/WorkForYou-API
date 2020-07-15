0bac79f056696b044fb3495734e17fdc7933f20d
radichev <unbreaking78@gmail.com>
2020-07-15 23:38:18 +0300

    Added method to find workSpehre by id

--- src/main/java/com/radichev/workforyou/service/WorkSphereService.java
+++ src/main/java/com/radichev/workforyou/service/WorkSphereService.java
@@ -1,4 +1,8 @@
 package com.radichev.workforyou.service;
 
+import com.radichev.workforyou.domain.entity.WorkSphere;
+
 public interface WorkSphereService {
+
+    WorkSphere findWorkSphereById(String workSphereId);
 }
--- src/main/java/com/radichev/workforyou/service/serviceImpl/WorkSphereServiceImpl.java
+++ src/main/java/com/radichev/workforyou/service/serviceImpl/WorkSphereServiceImpl.java
@@ -1,8 +1,20 @@
 package com.radichev.workforyou.service.serviceImpl;
 
+import com.radichev.workforyou.domain.entity.WorkSphere;
+import com.radichev.workforyou.repository.WorkSphereRepository;
 import com.radichev.workforyou.service.WorkSphereService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorkSphereServiceImpl implements WorkSphereService {
+    private final WorkSphereRepository workSphereRepository;
+
+    public WorkSphereServiceImpl(WorkSphereRepository workSphereRepository) {
+        this.workSphereRepository = workSphereRepository;
+    }
+
+    @Override
+    public WorkSphere findWorkSphereById(String workSphereId) {
+        return this.workSphereRepository.findById(workSphereId).get();
+    }
 }
